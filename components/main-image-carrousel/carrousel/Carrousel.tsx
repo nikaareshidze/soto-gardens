43db3d8ca112cabecb337fe9e03f9734b1e732b9
nikaareshidze <nikaareshidzeapps@gmail.com>
2023-05-09 13:58:48 +0400

    fix: change Carrousel components useEffect such as CallToAction

--- components/main-image-carrousel/carrousel/Carrousel.tsx
+++ components/main-image-carrousel/carrousel/Carrousel.tsx
@@ -1,27 +1,21 @@
 import { useEffect, useState } from "react";
 
-type ImageState = {
-  urls: string[];
-  currentIndex: number;
-};
-
 export default function Carrousel() {
-  const [imageState, setImageState] = useState<ImageState>({
+  const [imageState, setImageState] = useState({
     urls: [
       "https://cdn.shopify.com/s/files/1/0552/9331/3078/files/Soto_Green_Lifestyle_3000x3000.jpg?v=1649014077",
       "https://cdn.shopify.com/s/files/1/0552/9331/3078/files/SOTO_Classic_-_Lifestyle_02-4_3000x3000.jpg?v=1682370321",
       "https://cdn.shopify.com/s/files/1/0552/9331/3078/files/Soto_Product_Shoot_Low_Q-2746_3000x3000.jpg?v=1649793414",
       "https://cdn.shopify.com/s/files/1/0552/9331/3078/files/Big_Homepage_with_Black_test_4c6bb934-a31b-4c09-baf3-fb5fe55d082a_3000x3000.jpg?v=1648141933",
     ],
-    currentIndex: 0,
   });
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setImageState((prevState) => ({
-        ...prevState,
-        currentIndex: (prevState.currentIndex + 1) % prevState.urls.length,
-      }));
+      setCurrentIndex(
+        (currentIndex) => (currentIndex + 1) % imageState.urls.length
+      );
     }, 5000);
 
     return () => {
@@ -31,7 +25,7 @@ export default function Carrousel() {
 
   return (
     <img
-      src={imageState.urls[imageState.currentIndex]}
+      src={imageState.urls[currentIndex]}
       alt="house with garden"
       width="100%"
       height="640px"
