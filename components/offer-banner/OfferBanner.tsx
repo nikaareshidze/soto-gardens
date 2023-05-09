dcaa2b0cfe83add342963a1f82e84ceef6b82e2d
nikaareshidze <nikaareshidzeapps@gmail.com>
2023-05-09 13:09:06 +0400

    fix: offerBanner's text change every 5 second

--- components/offer-banner/OfferBanner.tsx
+++ components/offer-banner/OfferBanner.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 
 export default function OfferBanner() {
+  const [offerText, setOfferText] = useState<string[]>([
+    " Sign up to our newsletter for 10% off",
+    "Free delivery for orders from 50$",
+    "For our Border collections Soto hand deliver within Greater London and the home counties",
+  ]);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((currentIndex) => (currentIndex + 1) % offerText.length);
+    }, 5000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [offerText.length]);
+
   return (
     <div className={styles.offerBannerContainer}>
-      <h1 className={styles.offerBannerTitle}>
-        Sign up to our newsletter for 10% off
-      </h1>
+      <h1 className={styles.offerBannerTitle}>{offerText[currentIndex]} </h1>
     </div>
   );
 }
