0fbe5eea1f48b084c1a629e3061a93ed6631c70c
nikaareshidze <nikaareshidzeapps@gmail.com>
2023-05-09 17:24:48 +0400

    fix: logo

--- components/header/logo/Logo.tsx
+++ components/header/logo/Logo.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Logo() {
-  return <div>Logo</div>;
+  return (
+    <img
+      src="https://cdn.shopify.com/s/files/1/0552/9331/3078/t/7/assets/logo_3000x.png?v=134671221632750795351658431571"
+      width="107px"
+      height="30px"
+    />
+  );
 }
