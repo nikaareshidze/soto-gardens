0dc37ee9ee658ccdd6349958d176e84285404730
nikaareshidze <nikaareshidzeapps@gmail.com>
2023-05-09 13:51:43 +0400

    fix: change cta text every 5 seconds

--- components/main-image-carrousel/call-to-action/CallToAction.tsx
+++ components/main-image-carrousel/call-to-action/CallToAction.tsx
@@ -1,17 +1,54 @@
 import { ShopButton } from "@/components/shared";
+import { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 
 export default function CallToAction() {
+  const [ctaText, setCtaText] = useState([
+    {
+      titleFirstLine: "Welcome To",
+      titleSecondLine: "Soto",
+      descriptionFirstLine: "Professionally designed garden looks,",
+      descriptionSecondLine: "delivered to your door.",
+    },
+    {
+      titleFirstLine: "Explore Soto",
+      titleSecondLine: "House",
+      descriptionFirstLine: "",
+      descriptionSecondLine: "",
+    },
+    {
+      titleFirstLine: "Ronny Colbie",
+      titleSecondLine: "",
+      descriptionFirstLine: "Timeless, seasonal, traditional bouquets are",
+      descriptionSecondLine: "now available for national delivery.",
+    },
+  ]);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((currentIndex) => (currentIndex + 1) % ctaText.length);
+    }, 5000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [ctaText.length]);
+
   return (
     <div className={styles.callToAction}>
       <h1 className={styles.title}>
-        Welcome To <br />
-        Soto
+        {`${ctaText[currentIndex].titleFirstLine}`}
+        <br />
+        {`${ctaText[currentIndex].titleSecondLine}`}
       </h1>
-      <p className={styles.description}>
-        Professionally designed garden looks, <br />
-        delivered to your door.
-      </p>
+      {ctaText[currentIndex].descriptionFirstLine !== "" && (
+        <p className={styles.description}>
+          {`${ctaText[currentIndex].descriptionFirstLine}`}
+          <br />
+          {`${ctaText[currentIndex].descriptionSecondLine}`}
+        </p>
+      )}
       <ShopButton>Shop Now</ShopButton>
     </div>
   );
