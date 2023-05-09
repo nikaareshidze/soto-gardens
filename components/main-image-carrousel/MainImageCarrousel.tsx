a0b21b3801e1e1d525e50772af7a00a1a9de40da
nikaareshidze <nikaareshidzeapps@gmail.com>
2023-05-09 14:56:38 +0400

    add buttons to change currentIndex and images with clicks

--- components/main-image-carrousel/MainImageCarrousel.tsx
+++ components/main-image-carrousel/MainImageCarrousel.tsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import { CallToAction } from "./call-to-action";
 import { Carrousel } from "./carrousel";
 import styles from "./styles.module.scss";
+import { AiOutlineCaretRight } from "react-icons/ai";
+import { AiOutlineCaretLeft } from "react-icons/ai";
+import { RiArrowRightFill } from "react-icons/ri";
+import { RiArrowLeftFill } from "react-icons/ri";
 
 export default function MainImageCarrousel() {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -22,10 +26,67 @@ export default function MainImageCarrousel() {
     };
   });
 
+  const nextIndex = () => {
+    setCurrentIndex((prevIndex) => {
+      if (prevIndex === 3) {
+        return 0;
+      } else {
+        return prevIndex + 1;
+      }
+    });
+  };
+  const previousIndex = () => {
+    setCurrentIndex((prevIndex) => {
+      if (prevIndex === 0) {
+        return 3;
+      } else {
+        return prevIndex - 1;
+      }
+    });
+  };
+
+  const [leftBtnHover, setLeftBtnHover] = useState(false);
+  const [rightBtnHover, setRightBtnHover] = useState(false);
+
   return (
     <div className={styles.mainCarrouselContainer}>
       <CallToAction currentIndex={currentIndex} />
       <Carrousel currentIndex={currentIndex} />
+      <div className={styles.swipeButtons}>
+        <div
+          onMouseEnter={() => {
+            setLeftBtnHover(true);
+          }}
+          onMouseLeave={() => {
+            setLeftBtnHover(false);
+          }}
+        >
+          {leftBtnHover ? (
+            <RiArrowLeftFill size={20} color="white" onClick={nextIndex} />
+          ) : (
+            <AiOutlineCaretLeft size={20} color="white" onClick={nextIndex} />
+          )}
+        </div>
+
+        <div
+          onMouseEnter={() => {
+            setRightBtnHover(true);
+          }}
+          onMouseLeave={() => {
+            setRightBtnHover(false);
+          }}
+        >
+          {rightBtnHover ? (
+            <RiArrowRightFill size={20} color="white" onClick={nextIndex} />
+          ) : (
+            <AiOutlineCaretRight
+              size={20}
+              color="white"
+              onClick={previousIndex}
+            />
+          )}
+        </div>
+      </div>
     </div>
   );
 }
